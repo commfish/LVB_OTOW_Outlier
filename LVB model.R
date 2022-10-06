5c6d80b72fd8d24fcb4920551c498c2298b22f19
McNeel <kevin.mcneel@alaska.gov>
2022-10-06 10:25:24 -0800

    Correct naming to more general titles

--- LVB model.R
+++ LVB model.R
@@ -21,24 +21,23 @@ if(!require("ggplot2"))   install.packages("ggplot2")
 if(!require("tcltk2"))   install.packages("tcltk2")
 if(!require("tidyverse"))   install.packages("tidyverse")
 if(!require("readxl"))   install.packages("readxl")
+if(!require("plotly"))   install.packages("plotly")
 
 ##### Load data#########
 
-#dataBDRF4<-read_excel(file.choose()) 
-dataBDRF<-read_csv(file=file.choose())
-#dataBDRF<-read_excel(file.choose())
+dataBDRF4<-read_excel(file.choose()) 
+#dataBDRF<-read_csv(file=file.choose())
 #dataBDRF<-dataAll
 
 #Rename columns####
 #Rename the 'FIELDS' with the columns from your data to change them to the naming convention used in the script
 
-CN_age<-'age'
-CN_length<-'Total Length'
-CN_sex<-'GENDER_CODE'
-CN_species<-'FIELD_SPECIES_CODE'
-CN_otoW<-'AGE_STRUCTURE.WEIGHT'
-#dataBDRF$species<-rep('142',length(dataBDRF$SampleID))
-dataBDRF<- dataBDRF %>% rename(age=all_of(CN_age),length=all_of(CN_length),sex=all_of(CN_sex),species=all_of(CN_species),otoW=all_of(CN_otoW))
+CN_age<-'AGE'
+CN_length<-'LENGTH'
+CN_sex<-'SEX'
+CN_species<-'SPECIES'
+CN_otoW<-'OTOW'
+dataBDRF<- dataAll %>% rename(age=all_of(CN_age),length=all_of(CN_length),sex=all_of(CN_sex),species=all_of(CN_species),otoW=all_of(CN_otoW))
 #dataBDRF<- dataBDRF %>% rename(area='MANAGEMENT_AREA_CODE')
 if(max(dataBDRF$length,na.rm = TRUE)<200){
   dataBDRF$length<-dataBDRF$length*10 #convert cm to mm length
@@ -53,12 +52,11 @@ dataBRF<-subset(dataBDRF,dataBDRF$species==Tspecies) #filter data to target spec
 table(dataBRF$age)
 #####Plot data#######
 
-ggplot(dataBDRF,aes(otoW,length,col=as.factor(species)))+
-  geom_point(size=2)+
-  labs(col="Species")+
+ggplot(dataBDRF,aes(otoW,length,label=paste(SAMPLE," ",ID),col=as.factor(METHOD)),)+
+  geom_jitter(size=2)+
   theme_classic()
-
-#####Model######
+ggplotly()
+z#####Model######
 
 svTypical<-vbStarts(length~age,data=dataBRF,fixed=list(K=0.2,t0=-1),plot=TRUE,na.omit=TRUE)
 #svTypical <- list(Linf=530,K=0.09,t0=-1)# black rockfish starting values 
@@ -77,13 +75,11 @@ residPlot(fitTypical)
 
 overview(fitTypicalM)
 summary(fitTypicalM)
-write.table(summary(fitTypicalM)$coefficients, "clipboard", sep="\t",col.names=FALSE)
 fitPlot(fitTypicalM,xlab="Age",ylab="Total Length (mm)",main="")
 residPlot(fitTypicalM)
 
 overview(fitTypicalF)
 summary(fitTypicalF)
-write.table(summary(fitTypicalF)$coefficients, "clipboard", sep="\t",col.names=FALSE)
 fitPlot(fitTypicalF,xlab="Age",ylab="Total Length (mm)",main="")
 residPlot(fitTypicalF)
 
@@ -109,25 +105,20 @@ pred.predsumF$age<-seq(0,MaxAge,by=1)
 
 #Not sex specific
 #windows(5,4)
-plot(length ~ age, data=dataBRF,ylab= "Total Length (mm)",xlab="Age",pch=c(3),cex=0,ylim=c(200,max(na.omit(dataBDRF$length))),xlim=c(0,MaxAge))
+plot(length ~ age, data=dataBRF4,ylab= "Total Length (mm)",xlab="Age",pch=c(3),cex=0,ylim=c(200,max(na.omit(dataBDRF4$length))),xlim=c(0,MaxAge))
 polygon(c(pred.predsum$age, rev(pred.predsum$age)), c(pred.predsum[, 11],rev(pred.predsum[, 12])), col = "light blue",lty = 0)
-points(length ~ I(age+0.5), data=dataBDRF, subset= species!=Tspecies,col="red",pch=19,cex=1)#R1 Dusky
-points(length ~ I(age), data=dataBRF, col= "black" ,pch=19,cex=1)# R1 Black
+points(length ~ I(age+0.5), data=dataBDRF4, subset= species!=Tspecies,col="red",pch=19,cex=1)#R1 Dusky
+points(length ~ I(age), data=dataBRF4, col= "black" ,pch=19,cex=1)# R1 Black
 legend("bottomright",c("Black Rockfish","Other"),pch=c(19,19),col=c("black","red"),bty = "n")
 #savePlot("clipboard", type="wmf") #saves plot to WMF
 
 #Sex specific
 plot(length ~ age, data=dataBRF,ylab= "Total Length (mm)",xlab="Age",pch=c(3),cex=0,ylim=c(200,max(na.omit(dataBDRF$length))))
-points(length ~ I(age), data=dataBRF, subset=sex==c("M","1"),col="lightblue",pch=19,cex=1)
-points(length ~ I(age), data=dataBRF, subset=sex==c("F","2"),col="red",pch=19,cex=1)
-points(length ~ I(age), data=dataBRF, subset=sex==c("NA","9"),col="yellow",pch=19,cex=1)
-
+points(length ~ I(age), data=dataBRF, subset=sex==c("M","1"),col=rgb(red=0,green=0,blue=0.8,alpha=0.2),pch=19,cex=1)
+points(length ~ I(age-0.3), data=dataBRF, subset=sex==c("F","2"),col=rgb(red=0.8,green=0,blue=0,alpha=0.2),pch=19,cex=1)
 polygon(c(pred.predsumM$age, rev(pred.predsumM$age)), c(pred.predM$summary[, 11],rev(pred.predM$summary[, 12])), col = rgb(red=0,green=0,blue=.6,alpha=0.2),lty = 0)
 polygon(c(pred.predsumF$age, rev(pred.predsumF$age)), c(pred.predF$summary[, 11],rev(pred.predF$summary[, 12])), col = rgb(red=.6,green=0,blue=0,alpha=0.2),lty = 0)
 points(length ~ I(age+0.2), data=dataBDRF, subset= species!=Tspecies,col=rgb(red=0,green=0,blue=0,alpha=0.2),pch=19,cex=1)#R1 Dusky
-table(dataBDRF$sex,useNA = "always")
-
-
 ####################Export Table################################################
 write.table(pred.predsum, "clipboard", sep="\t", row.names=FALSE,col.names = FALSE)
 write.table(pred.predsumM, "clipboard", sep="\t", row.names=FALSE)
